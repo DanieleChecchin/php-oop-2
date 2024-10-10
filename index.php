3a0e77ade8968d6cb4fc968cdb17229c4a76b3cd
DanieleChecchin <danielechecchin26@gmail.com>
2024-10-10 18:20:37 +0200

    card con immagini

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ $dog = new Categories('Cane');
 $cat = new Categories('Gatto');
 
 // CIBO
-$food1 = new Foods('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.obi-italia.it%2Farticoli-per-animali%2Farticoli-per-cani%2Fc%2F3747&psig=AOvVaw3KwR5v52CJiQ5Mv3fnjQmd&ust=1728657964735000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLjknvqGhIkDFQAAAAAdAAAAABAE', 'Special Food', 7.99, 'Cibo', $dog, 'Cani adulti');
-$food2 = new Foods('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dm-drogeriemarkt.it%2Fwhiskas-croccantini-per-gatti-adulti-con-tonno-p5900951283734.html&psig=AOvVaw2h294smF8Df7-qDwNTtbbf&ust=1728659137711000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJCn_KGLhIkDFQAAAAAdAAAAABAE', 'Whiskas', 5.99, 'Cibo', $cat, 'Gatti cuccioli');
+$food1 = new Foods('https://m.media-amazon.com/images/I/41Y35B127wL._AC_UF894,1000_QL80_.jpg', 'Special Dog', 7.99, 'Cibo', $dog, 'Cani adulti');
+$food2 = new Foods('https://fera24.it/images/thumbnails/960/1214/detailed/129/5900951299209F1.jpg', 'Whiskas', 5.99, 'Cibo', $cat, 'Gatti cuccioli');
 
 $foods = [
     $food1,
@@ -20,8 +20,8 @@ $foods = [
 ];
 
 // CUCCE
-$petBed1 = new PetBeds('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.satur.it%2Fcuccia-jeans-estiva-46x38-cm-in-oxford-duppid-249-5907036.html&psig=AOvVaw0C7W1ZdoWmx4dr8PLX0zjj&ust=1728660092337000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPjS_eeOhIkDFQAAAAAdAAAAABAE', 'Satur', 25.99, 'Cuccia', $dog, 'Grigia', 'Estiva');
-$petBed2 = new PetBeds('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.it%2FImpermeabile-Resistente-Intemperie-Forniture-60x43x40cm%2Fdp%2FB0CPDXVJJN&psig=AOvVaw0xY9kIiZAoUbUiZHaN76X4&ust=1728660545557000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOimqMKQhIkDFQAAAAAdAAAAABAE', 'Cpaps', 38.99, 'Cuccia', $cat, 'Nero', 'Invernale');
+$petBed1 = new PetBeds('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWWu3l-P-c_5OEEW6J-sxi3E3ZqTwrC4maPw&s', 'Satur', 25.99, 'Cuccia', $dog, 'Grigia', 'Estiva');
+$petBed2 = new PetBeds('https://m.media-amazon.com/images/I/71cS+MII06L._AC_UF350,350_QL80_.jpg', 'Cpaps', 38.99, 'Cuccia', $cat, 'Nero', 'Invernale');
 
 $petBeds = [
     $petBed1,
@@ -29,8 +29,8 @@ $petBeds = [
 ];
 
 // GIOCATTOLI
-$toy1 = new AnimalToys('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.arcaplanet.it%2Fgioco-cane-peluche-corda-tigre%2Fp&psig=AOvVaw3pwi_DFRoIJW64kx3xZMbL&ust=1728661083562000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPCyn8KShIkDFQAAAAAdAAAAABAE', 'Lovedi', 15.99, 'Giocattolo', $dog, 'Marrone', 0.300);
-$toy2 = new AnimalToys('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.zoomalia.it%2Fnegozio-di-animali%2Fgiocattolo-gatto-duo-topo-con-piume-5-cm-p-29815.html&psig=AOvVaw1yldhsVLRNfiQ_ibfim6vM&ust=1728661249216000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMjMjJGThIkDFQAAAAAdAAAAABAE', 'Zolux', 22.99, 'Giocattolo', $cat, 'Bianco e Nero', 0.150);
+$toy1 = new AnimalToys('https://arcaplanet.vtexassets.com/arquivos/ids/283187/FP4.1.307-AP--1-.jpg?v=638158653329870000', 'Lovedi', 15.99, 'Giocattolo', $dog, 'Marrone', 0.300);
+$toy2 = new AnimalToys('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfixc7loJc5-06DKyFRzKAmSBf8MJNd8QVEA&s', 'Zolux', 22.99, 'Giocattolo', $cat, 'Bianco e Nero', 0.150);
 
 $toys = [
     $toy1,
@@ -58,7 +58,7 @@ $toys = [
             </div>
             <?php foreach ($foods as $singleFood) { ?>
             <div class="card col-6" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="<?= $singleFood->getImage() ?>" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title"> <?= $singleFood->getTitle(); ?> </h5>
                     <p class="card-text"> <?= $singleFood->getPrice(); ?> </p>
@@ -71,7 +71,7 @@ $toys = [
 
             <?php foreach ($petBeds as $singlePetBed) { ?>
             <div class="card col-6" style="width: 18rem;">
-                <img <?= $singlePetBed->getImage(); ?> class="card-img-top" alt="...">
+                <img src="<?= $singlePetBed->getImage() ?>" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title"> <?= $singlePetBed->getTitle(); ?> </h5>
                     <p class="card-text"> <?= $singlePetBed->getPrice(); ?> </p>
@@ -85,7 +85,7 @@ $toys = [
 
             <?php foreach ($toys as $singleToy) { ?>
             <div class="card col-6" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
+                <img src=" <?= $singleToy->getImage() ?> " class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title"> <?= $singleToy->getTitle(); ?> </h5>
                     <p class="card-text"> <?= $singleToy->getPrice(); ?> </p>
